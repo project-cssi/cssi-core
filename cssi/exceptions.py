a70f08de2e5726c708a3bc1aba1205e532c5ec96
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-19 21:45:09 +0530

    chore(exceptions): add base exceptions for CSSI

---
+++ cssi/exceptions.py
@@ -0,0 +1,8 @@
+class BaseCSSIException(Exception):
+    """The base of all CSSI library exceptions."""
+    pass
+
+
+class CSSIException(BaseCSSIException):
+    """An exception specific to CSSI library"""
+    pass
