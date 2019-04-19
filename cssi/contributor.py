320266238e4f95c294c3b5dfc2084724f46718f0
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-19 22:32:12 +0530

    chore(core): implement base interface for all the contributors

---
+++ cssi/contributor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# (c) Copyright 2019 Brion Mario.
+# (c) This file is part of the CSSI Core library and is made available under MIT license.
+# (c) For more information, see https://github.com/brionmario/cssi-core/blob/master/LICENSE.txt
+# (c) Please forward any queries to the given email address. email: brion@apareciumlabs.com
+
+"""Interface for all the CSSI contributors
+
+All the contributors of CSSI score generation should implement this
+class.
+
+Authors:
+    Brion Mario
+
+"""
+
+from cssi.misc import _must_be_implemented
+
+
+class CSSIContributor(object):
+
+    def score(self):
+        _must_be_implemented(self, "score")
