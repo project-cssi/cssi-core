7ee2b0b566e1cd467415371d4684ad24182a1fc7
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-19 23:51:40 +0530

    chore(misc): modify _must_be_implemented function's string formatting

--- cssi/misc.py
+++ cssi/misc.py
@@ -1,15 +1,15 @@
 def _must_be_implemented(that, func_name):
     """Helper function to raise NotImplementedError in interfaces."""
-    if hasattr(that, "_cssi_plugin_name"):
-        item = "Plugin"
+    if hasattr(that, '_cssi_plugin_name'):
+        item = 'Plugin'
         name = that._cssi_plugin_name
     else:
-        item = "Class"
+        item = 'Class'
         _class = that.__class__
-        name = "{_class.__module__}.{_class.__name__}".format(_class=_class)
+        name = '{_class.__module__}.{_class.__name__}'.format(_class=_class)
 
     raise NotImplementedError(
-        "{item} {name!r} needs to implement the {func_name}() function".format(
+        '{item} {name!r} needs to implement the {func_name}() function'.format(
             item=item, name=name, func_name=func_name
             )
         )
