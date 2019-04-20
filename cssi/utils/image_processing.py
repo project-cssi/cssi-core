97407e47a27cea4e76d5223234cbb800097a19aa
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-20 18:17:04 +0530

    chore(utils): add inverse to euler function

--- cssi/utils/image_processing.py
+++ cssi/utils/image_processing.py
@@ -11,13 +11,23 @@ def calculate_euler_angles(R, inverse=False):
     singular = sy < 1e-6
 
     if not singular:
-        x = math.atan2(R[2, 1], R[2, 2])
-        y = math.atan2(-R[2, 0], sy)
-        z = math.atan2(R[1, 0], R[0, 0])
+        if inverse:
+            x = math.atan2(-R[2, 1], R[2, 2])
+            y = math.atan2(R[2, 0], sy)
+            z = math.atan2(-R[1, 0], R[0, 0])
+        else:
+            x = math.atan2(R[2, 1], R[2, 2])
+            y = math.atan2(-R[2, 0], sy)
+            z = math.atan2(R[1, 0], R[0, 0])
     else:
-        x = math.atan2(-R[1, 2], R[1, 1])
-        y = math.atan2(-R[2, 0], sy)
-        z = 0
+        if inverse:
+            x = math.atan2(R[1, 2], R[1, 1])
+            y = math.atan2(R[2, 0], sy)
+            z = 0
+        else:
+            x = math.atan2(-R[1, 2], R[1, 1])
+            y = math.atan2(-R[2, 0], sy)
+            z = 0
 
     x = x * 180.0 / math.pi
     y = y * 180.0 / math.pi
