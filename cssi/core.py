a11bdda77600f68e526e0a8e6c7573acc58f58aa
Brion Mario <brion.2015283@iit.ac.lk>
2019-04-27 23:26:57 +0530

    feat(core): implement initial version of the CSSI algorithm

--- cssi/core.py
+++ cssi/core.py
@@ -14,6 +14,7 @@ Authors:
 """
 import os
 import logging
+
 # `Keras` was hanging when trying to use the model.predict
 # and swapping the backend from `Tensorflow` to `Theano`
 # fixed the issue.
@@ -23,6 +24,7 @@ from cssi.config import read_cssi_config
 from cssi.latency import Latency
 from cssi.sentiment import Sentiment
 from cssi.questionnaire import SSQ
+from cssi.exceptions import CSSIException
 
 logger = logging.getLogger(__name__)
 
@@ -52,3 +54,65 @@ class CSSI(object):
         # Initializing the questionnaire module.
         self.questionnaire = SSQ(config=self.config, debug=self.debug)
         logger.debug("CSSI library initialized......")
+
+    def generate_cssi_score(self, tl, ts, tq, plugin_scores=None):
+        """Generators the final CSSI score.
+
+        This is the core function of the CSSI library and it takes in the scores and
+        generates the final CSSI score for the test session.
+
+        Args:
+            tl (float): Total latency score
+            ts (float): Total sentiment score
+            tq (float): Total questionnaire score
+            plugin_scores (list): A list of dictionaries containing plugin details.
+                ex: [{"name": "heartrate.plugin", "score": 40.00}].
+        Returns:
+            float: The CSSI score.
+        Raises:
+            CSSIException: If the calculations couldn't be completed successfully
+                this exception will be thrown.
+        Examples:
+            >>> cssi.generate_cssi_score(tl, ts, tq, plugin_scores)
+        """
+        tot_ps = 0.0  # Variable to store the sum of the plugin scores
+        tot_pw = 0  # Variable to keep track total plugin weight
+
+        # Checks if any plugins are provided for score calculation.
+        if plugin_scores is not None:
+            for plugin in plugin_scores:
+                plugin_name = plugin["name"]
+                # Checks if the plugin is registered in the configuration file
+                # If not, raises an exception.
+                if plugin_name not in self.config.plugins:
+                    raise CSSIException("The plugin {0} appears to be invalid.".format(plugin_name))
+                else:
+                    plugin_weight = float(self.config.plugin_options[plugin_name]["weight"]) / 100
+                    plugin_score = plugin["score"]
+
+                    # Checks if the passed in plugin score is less than 100.
+                    # If not an exception will be thrown.
+                    if plugin_score > 100:
+                        raise CSSIException("Invalid score provided for the plugin: {0}.".format(plugin_name))
+
+                    # Ads the current plugin score to the total plugin score.
+                    tot_ps += plugin_score * plugin_weight
+                    # Ads the current plugin weight to the total plugin weight percentage.
+                    tot_pw += plugin_weight
+
+        lw = float(self.config.latency_weight) / 100  # latency weight percentage
+        sw = float(self.config.sentiment_weight) / 100  # sentiment weight percentage
+        qw = float(self.config.questionnaire_weight) / 100  # questionnaire weight percentage
+
+        # Checks if the total weight is less than 100 percent.
+        if (lw + sw + qw + tot_pw) > 1:
+            raise CSSIException("Invalid weight configuration. Please reconfigure and try again")
+
+        # Calculating the CSSI score
+        cssi = (tl * lw) + (ts * sw) + (tq * qw) + tot_ps
+
+        # Double checks if the generated CSSI score is less than 100.
+        if cssi > 100:
+            raise CSSIException("Invalid CSSI score was generated. Please try again")
+
+        return cssi
