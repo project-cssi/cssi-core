c910e2644fc232dc781995488f2f5b10b70bc629
Brion Mario <brion.2015283@iit.ac.lk>
2019-05-16 13:42:44 +0530

    chore(core): implement plugin init function

--- cssi/plugin_manager.py
+++ cssi/plugin_manager.py
@@ -1,4 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# (c) Copyright 2019 Brion Mario.
+# (c) This file is part of the CSSI Core library and is made available under MIT license.
+# (c) For more information, see https://github.com/brionmario/cssi-core/blob/master/LICENSE.txt
+# (c) Please forward any queries to the given email address. email: brion@apareciumlabs.com
+
+"""This module provides plugin support for the CSSI Library
+
+Authors:
+    Brion Mario
+
+"""
+
+import sys
+
+from cssi.exceptions import CSSIException
+
+
 class Plugins:
-    @classmethod
-    def init_plugins(cls, modules, config, debug):
+    """Manages all the CSSI plugins"""
+
+    def __init__(self):
         pass
+
+    @classmethod
+    def init_plugins(cls, modules, config):
+        """Load the plugins"""
+        plugins = cls()
+
+        for module in modules:
+            plugins.current_module = module
+            __import__(module)
+            mod = sys.modules[module]
+
+            cssi_init = getattr(mod, "cssi_init", None)
+            if not cssi_init:
+                raise CSSIException(
+                    "The plugin module {0} doesn't contain a cssi_init function".format(module)
+                )
+
+            options = config.get_plugin_options(module)
+            cssi_init(plugins, options)
+
+        plugins.current_module = None
+        return plugins
+
+
+
